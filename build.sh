cba7bfacb2d0775a37c7b26f658fd983871de945
Joshua Krusell <js.shirin@gmail.com>
2016-02-25 17:45:03 +0100

    update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/zsh
 
 mkdir lib 2>/dev/null
-[[ ${(L)1} == "all" ]] && rm lib/*.js(N)
+
+case ${(L)1} in
+    ("all"|"clean")
+        rm lib/*.js(N) 2>/dev/null ;|
+    ("clean")
+        exit;;
+esac
+
 for i in src/*.js; do
     out_file="lib/${i:t}"
     if [[ ! -f $out_file || $i -nt $out_file ]]; then
