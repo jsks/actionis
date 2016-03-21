efd4393e876cd52176d54b411ba7389949f4bf04
Joshua Krusell <js.shirin@gmail.com>
2016-03-21 12:50:41 +0100

    clean up entry

--- src/actionis.js
+++ src/actionis.js
@@ -1,28 +1,26 @@
 'use strict'
 
 const fs = require('fs'),
-    os = require('os'),
-    commands = require('./commands.js'),
-    activity = require('./activity.js')
+      os = require('os'),
+      commands = require('./commands.js'),
+      activity = require('./activity.js')
 
-const jsonFile = `${os.homedir()}/.local/actionis/log.json`,
-      argv = process.argv.slice(2),
-      cmd = argv[0]
+const jsonFile = `${os.homedir()}/.local/actionis/log.json`
 
 module.exports.exec = function(args) {
+    const cmd = args[0]
+
     if (!cmd) {
         console.log(Object.keys(commands).sort().join(' '))
         process.exit(0)
-    }
-
-    if (Object.keys(commands).indexOf(cmd) == -1) {
+    } else if (Object.keys(commands).indexOf(cmd) == -1) {
         console.error('Invalid command')
         process.exit(1)
     }
 
     fs.readFile(jsonFile, function(err, data) {
         const log = activity(JSON.parse(data)),
-              out = commands[cmd](log, argv.slice(1), jsonFile)
+              out = commands[cmd](log, args.slice(1))
 
         if (out)
             fs.writeFileSync(jsonFile, JSON.stringify(out))
