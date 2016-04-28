e73ee517ffb5978f3d3ba81584428154c3c7a9d0
Joshua Krusell <js.shirin@gmail.com>
2016-04-28 21:56:57 +0200

    remove email option from reporter

--- bin/reporter.sh
+++ bin/reporter.sh
@@ -4,13 +4,9 @@
 setopt err_return
 
 if [[ -z $1 ]]; then
-    print "No recipient."
-    print "USAGE: ./actionis-report <email address>"
+    print "No output file specified."
+    print "USAGE: ./actionis-report <output html-file>"
     exit 1
 fi
 
-OUTPUT=$(mktemp /tmp/report-XXXX.html)
-
-Rscript -e "rmarkdown::render(\"${0:A:h}/../R/report.Rmd\", output_file=\"$OUTPUT\", intermediates_dir=\"/tmp\", quiet=T)"
-print "Attached.\n\nBest,\nactionis" | mutt -s "Actionis: Weekly Report" -a $OUTPUT -- $1
-rm $OUTPUT
+Rscript -e "rmarkdown::render(\"${0:A:h}/../R/report.Rmd\", output_file=\"$1\", intermediates_dir=\"/tmp\", quiet=T)"
